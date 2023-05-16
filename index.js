5a7805c50182ed8845ace4a10b206179f400f1c6
cisco-home-pc <feelgeel@gmail.com>
2023-05-16 21:14:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser'); 
+const port = process.env.PORT || 3002;
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+//=================================
+//              USERS
+//=================================
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true
+        })
+    })
+});
+app.listen(port,()=>{
+    console.log(`Server Running at ${port}`)
+})
